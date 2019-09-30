25732e23d83fb6f87c11b7f30351374bbdf383bc
Ubuntu <ubuntu@ip-172-31-30-175.us-east-2.compute.internal>
2019-09-30 23:38:59 +0000

    adds fetch, state, and events map. WIP fetching API

--- app/javascript/components/MainApp.js
+++ app/javascript/components/MainApp.js
@@ -9,6 +9,25 @@ import Home from './Home'
 import Event from './Event'
 
 class MainApp extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      events: []
+    }
+    this.getEvent()
+  }
+  
+  getEvent = () => {
+    /* global fetch */
+    fetch("../events")
+    .then(response => {
+      return response.json()
+    })
+    .then(events => {
+      this.setState({events})
+    })
+  }
+  
   render () {
     const {
       logged_in,
@@ -21,13 +40,23 @@ class MainApp extends React.Component {
       <React.Fragment>
         <BrowserRouter>
           <Switch>
-          <Route path = "/Event" component={Event} />
+          
                 
+          <Route path = '/Event' render = {(routeProps) => {
+            return(
+              <Event {...routeProps} events={this.state.events} />
+              )
+            }} 
+          />
                       
           <Route exact path="/" render={(routeProps)=> {
               return(
-                <Home {...routeProps} sign_in_route={this.props.sign_in_route} />)}
-                } />
+                <Home {...routeProps} sign_in_route={this.props.sign_in_route} />
+              )
+            }} 
+          />
+                
+                
                   {logged_in &&
                     <div>
                       <a href={sign_out_route}>Sign Out</a>

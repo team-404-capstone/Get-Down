e9fafc3f1c26f6ccc837f9b21754be71fddd5812
Ubuntu <ubuntu@ip-172-31-30-175.us-east-2.compute.internal>
2019-10-02 16:30:03 +0000

    adds files that were lost in a merge conflict

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,33 @@
+class EventsController < ApplicationController
+    
+    def index
+        events = Event.all
+        render json: events
+    end
+    
+    def update
+        event = Event.update post_params
+        render json: event
+    end
+    
+    def show
+    end
+    
+    def create
+        event = Event.create post_params
+        render json: event, status: 201
+    end
+    
+    def destroy
+        event = Event.find params[:id]
+        if event.destroy
+            render json: event
+        end
+    end
+    
+    private
+    
+    def post_params
+        params.require(:event).permit(:name, :date, :time, :lat, :lng, :description)
+    end
+end

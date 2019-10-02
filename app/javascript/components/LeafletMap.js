1695c217bce360da7a2ae0315b623366643c9077
adamjacobcruse@gmail.com <learn@learnT7.local>
2019-10-02 16:47:22 -0700

    leaflet indentation

--- app/javascript/components/LeafletMap.js
+++ app/javascript/components/LeafletMap.js
@@ -5,20 +5,21 @@ import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet'
 
 
 export default class MyMap extends Component{
-    constructor(props){
-        super(props)
-        this.state ={
-            lat: 51.505,
-            lng: -0.09,
-            zoom: 13
-        }
-    }
-    render(){
-        const position = [this.state.lat, this.state.lng]
-        return(
-            <div >
-            <LeafletMap
-            style={{height:500, width:500}}
+  constructor(props){
+    super(props)
+    this.state ={
+      lat: 32.7157,
+      lng: -117.1611,
+      zoom: 13
+      }
+  }
+  render(){
+    const position = [this.state.lat, this.state.lng]
+      return(
+        <div>
+        <center>
+          <LeafletMap
+            style={{height:500, width:750}}
             center={position}
             zoom={this.state.zoom}
             maxZoom={20}
@@ -29,15 +30,14 @@ export default class MyMap extends Component{
             dragging={true}
             animate={true}
             easeLinearity={0.35}
-             >
-             <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
-        />
+          >
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+          />
           </LeafletMap>
-
-            <h1> hello</h1>
-            </div>
+        </center>
+        </div>
         )
     }
 }

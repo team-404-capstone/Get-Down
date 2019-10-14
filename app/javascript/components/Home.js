f2df0ca7c593cee95f67ced0ed42f864a22ffdc2
Ubuntu <ubuntu@ip-172-31-30-175.us-east-2.compute.internal>
2019-10-14 23:09:14 +0000

    deleted commented code

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -203,29 +203,3 @@ class Home extends React.Component {
 
 export default Home
 
-
-// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ORIGINAL NAVBAR
-// <nav class="navbar navbar-expand-md navbar-dark bg-primary mb-2">
-//       	<a class="navbar-brand" href="./events">Light text</a>
-//       	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-//       		<span class="navbar-toggler-icon"></span>
-//       	</button>
-//       	<div class="collapse navbar-collapse" id="navbarColor02">
-//       		<ul class="navbar-nav ml-auto">
-//       			<li class="nav-item">
-//       				<a class="nav-link" href="#" title="Current breakpoint tier">
-//       					Tier
-//       					<span class="d-xl-inline d-none">XL</span>
-//       					<span class="d-lg-inline d-xl-none d-none">LG</span>
-//       					<span class="d-md-inline d-lg-none d-none">MD</span>
-//       					<span class="d-sm-inline d-md-none d-none">SM</span>
-//       					<span class="d-inline d-sm-none">XS</span>
-//       				</a>
-//       			</li>
-//       			<li class="nav-item">
-//                 <a class="nav-link" href="/users/sign_up">Sign Up</a>
-//                 <a class="nav-link" href={sign_in_route}>Log In</a>
-//             </li>
-//       		</ul>
-//       	</div>
-//       </nav>

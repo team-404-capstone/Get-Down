078147f1e3b484c2c2121db39453cefdd8e156a6
Ubuntu <ubuntu@ip-172-31-30-175.us-east-2.compute.internal>
2019-10-14 17:21:23 +0000

    adds username to devise, sign up page and fixed the view for attendees and comments. It shows username now not the email address.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
     before_action :configure_devise_params, if: :devise_controller?
     skip_before_action :verify_authenticity_token
 
+    protected
+
     def configure_devise_params
       devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-  end
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :events
   has_many :attends
   has_many :comments
+  
+  validates :username, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,

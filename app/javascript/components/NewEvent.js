4d592defb894c3c6f72e37154d855977231f4fc2
adamjacobcruse@gmail.com <learn@learnT7.local>
2019-10-15 16:58:26 -0700

    added changes to styling

--- app/javascript/components/NewEvent.js
+++ app/javascript/components/NewEvent.js
@@ -51,23 +51,23 @@ class NewEvent extends React.Component {
               <Container>
               <Form>
                 <FormGroup>
-                  <Label for="name">Event Name</Label>
+                  <Label style={{color: "black"}} for="name">Event Name</Label>
                   <Input onChange = { this.onChange } id="name"  type="string" name="name"placeholder="Event Name" />
                 </FormGroup>
                 <FormGroup>
-                  <Label for="date">Date</Label>
+                  <Label style={{color: "black"}} for="date">Date</Label>
                   <Input onChange = { this.onChange } id="date"  type="date" name="date" placeholder="Date" />
                 </FormGroup>
                 <FormGroup>
-                  <Label for="time">Time</Label>
+                  <Label style={{color: "black"}} for="time">Time</Label>
                   <Input onChange = { this.onChange }  id="time" type="text" name="time" placeholder="Time" />
                 </FormGroup>
                 <FormGroup>
-                  <Label for="description">Description</Label>
+                  <Label style={{color: "black"}}  for="description">Description</Label>
                   <Input onChange = { this.onChange } id="description" type="text" name="description" placeholder="Description" />
                 </FormGroup>
                 <FormGroup>
-                  <Label for="address">Address</Label>
+                  <Label style={{color: "black"}} for="address">Address</Label>
                   <Input onChange = { this.onChange } id="address" type="address" name="address" placeholder="Enter address" />
                 </FormGroup>
                 <Button onClick = {this.localSubmit} >Create</Button>
